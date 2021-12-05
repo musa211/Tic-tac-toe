2f2158ed4fef576d22f941577e8f9a2f47c61edb
musa211 <mjean211@fiu.edu>
2021-12-04 23:14:40 -0500

    Vertical and horizontal wins

--- src/components/Game.js
+++ src/components/Game.js
@@ -78,19 +78,44 @@ export default class Game extends Component {
 
 function calculateWinner(squares) {
     const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        // Horizontal wins
+        [0, 1, 2, 3],       
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [6, 7, 8, 9],
+        [10, 11, 12, 13],
+        [11, 12, 13, 14],
+        [15, 16, 17, 18],
+        [16, 17, 18, 19],
+        [20, 21, 22, 23],
+        [21, 22, 23, 24],
+        // Vertical wins
+        [0, 5, 10, 15],
+        [5, 10, 15, 20],
+        [1, 6, 11, 16],
+        [6, 11, 16, 21],
+        [2, 7, 12, 17],
+        [7, 12, 17, 22],
+        [3, 8, 13, 18],
+        [8, 13, 18, 23],
+        [4, 9, 14, 19],
+        [9, 14, 19, 24],
+        // Diagonal Wins
+        [0, 6, 12, 18],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+
+
+
     ];
 
     for (let i = 0 ; i <  lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+        const [a, b, c, d] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c] && squares[c] === squares[d]) {
             return squares[a];
         }
     }
