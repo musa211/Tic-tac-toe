517c9ed12cf4f9875326c0f4f36c169af7e49a0d
musa211 <mjean211@fiu.edu>
2021-12-04 22:57:03 -0500

    Layout is done

--- src/components/Board.js
+++ src/components/Board.js
@@ -14,31 +14,36 @@ export default class Board extends Component {
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="border-row">
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
+                </div>
+                <div className="border-row">
                     {this.renderSquare(5)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="border-row">
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(9)}
                 </div>
                 <div className="border-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(10)}
+                    {this.renderSquare(11)}
+                    {this.renderSquare(12)}
+                    {this.renderSquare(13)}
+                    {this.renderSquare(14)}
                 </div>
                 <div className="border-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                    {this.renderSquare(2)}
+                    {this.renderSquare(15)}
+                    {this.renderSquare(16)}
+                    {this.renderSquare(17)}
+                    {this.renderSquare(18)}
+                    {this.renderSquare(19)}
+                </div>
+                <div className="border-row">
+                    {this.renderSquare(20)}
+                    {this.renderSquare(21)}
+                    {this.renderSquare(22)}
+                    {this.renderSquare(23)}
+                    {this.renderSquare(24)}
                 </div>
             </div>
         )
